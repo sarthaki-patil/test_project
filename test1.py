c740409a02ae875fda35db9a5b6307fadf87a67e
Sarthaki Patil <sarthaki.patil@calsoftinc.com>
2023-05-25 15:07:36 +0530

    employee class created in test1

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
 print("Hello")
+
+class Employee:
+    def __init__(self,name,location,salary):
+        self.emp_name = name
+        self.emp_location = location
+        self.emp_salary = salary
+
+    def get_emp_info(self):
+        print(f"""Employee Name :- {self.emp_name}
+Employee Location :- {self.emp_location}
+Employee Salary :- {self.emp_salary}""")
+
+e = Employee("Ram","Pune",20000)
+e.get_emp_info()
